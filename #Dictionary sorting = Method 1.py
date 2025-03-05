eb6bfe5aca0af9e132e44aed953c8947b84f672f
Janani Nasiyanur Thankarasu <112631597+jananinasiyanurthankarasu@users.noreply.github.com>
2025-03-05 18:57:53 +0000

    Add files via upload

---
+++ #Dictionary sorting = Method 1.py
@@ -0,0 +1,13 @@
+#Dictionary sorting = Method 1
+
+employees = {
+    "John" :1,
+    "Jane" : 2,
+    "Bob" : 3,
+    "Janani" :6,
+    "Raghu" : 1, 
+    "Alfia" :5
+}
+
+
+print(dict(sorted(employees.items())))
